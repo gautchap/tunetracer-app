02de9a96a559d2041374d911d69a54ca98681fa6
gautchap <gautchap+github@outlook.com>
2023-07-23 01:26:56 +0200

    refactor : add refreshToken hook and change types

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,41 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth, { NextAuthOptions, TokenSet } from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
+import { JWT } from "next-auth/jwt";
+
+const refreshAccessToken = async (token: JWT): Promise<JWT | undefined> => {
+    try {
+        const response = await fetch("https://accounts.spotify.com/api/token", {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                Authorization: `Basic ${Buffer.from(
+                    `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
+                ).toString("base64")}`,
+            },
+
+            body: new URLSearchParams({
+                grant_type: "refresh_token",
+                refresh_token: token.refreshToken,
+            }),
+            method: "POST",
+        });
+
+        const tokens: TokenSet = await response.json();
+
+        if (!response.ok) throw tokens;
+
+        token = Object.assign({}, token, {
+            accessToken: tokens.access_token,
+        });
+        token = Object.assign({}, token, {
+            accessTokenExpires: tokens.expires_at && Math.floor(Date.now() / 1000 + tokens.expires_at),
+        });
+        token = Object.assign({}, token, {
+            refreshToken: tokens.refresh_token,
+        });
+    } catch {
+        return { ...token, error: "RefreshAccessTokenError" as const };
+    }
+};
 
 export const authOptions: NextAuthOptions = {
     providers: [
@@ -16,20 +52,36 @@ export const authOptions: NextAuthOptions = {
     ],
     callbacks: {
         async session({ session, token }) {
-            if (session) {
+            if (session && token) {
                 session = Object.assign({}, session, { id: token.id });
-                session = Object.assign({}, session, { access_token: token.access_token });
+                session = Object.assign({}, session, {
+                    accessToken: token.accessToken,
+                });
+                session = Object.assign({}, session, { error: token.error });
             }
             return session;
         },
+
         async jwt({ token, user, account }) {
             if (user) {
                 token = Object.assign({}, token, { id: user.id });
             }
 
             if (account) {
-                token = Object.assign({}, token, { access_token: account.access_token });
-                token = Object.assign({}, token, { refresh_token: account.refresh_token });
+                token = Object.assign({}, token, {
+                    accessToken: account.access_token,
+                });
+                token = Object.assign({}, token, {
+                    accessTokenExpires: account.expires_at && Math.floor(Date.now() / 1000 + account.expires_at),
+                });
+                token = Object.assign({}, token, {
+                    refreshToken: account.refresh_token,
+                });
+            }
+
+            if (token.accessTokenExpires && Date.now() * 1000 > token.accessTokenExpires * 1000) {
+                const newToken = await refreshAccessToken(token);
+                if (newToken) return newToken;
             }
 
             return token;
--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,15 +1,22 @@
-import NextAuth from "next-auth";
-import { JWT } from "next-auth/jwt";
+import { DefaultSession, Account as NextAuthAccount } from "next-auth";
+import { JWT as NextAuthJWT } from "next-auth/jwt";
 
 declare module "next-auth" {
-    interface Session {
-        access_token: string;
+    interface Session extends DefaultSession {
+        accessToken: string;
+        error?: string;
+    }
+    interface Account extends NextAuthAccount {
+        expires_at: number;
     }
 }
 
 declare module "next-auth/jwt" {
-    interface JWT {
+    interface JWT extends NextAuthJWT {
         id: string;
-        access_token: string;
+        accessToken: string;
+        refreshToken: string;
+        accessTokenExpires?: number;
+        error?: "RefreshAccessTokenError";
     }
 }
