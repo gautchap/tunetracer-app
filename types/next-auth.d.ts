321062776ac3b7313198612514f1114e8f8d8c5b
gautchap <gautchap+github@outlook.com>
2023-07-22 23:20:26 +0200

    feat : add next auth types

---
+++ types/next-auth.d.ts
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+import { JWT } from "next-auth/jwt";
+
+declare module "next-auth" {
+    interface Session {
+        access_token: string;
+    }
+}
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        id: string;
+        access_token: string;
+    }
+}
