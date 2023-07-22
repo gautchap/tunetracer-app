df745acc038432f42c19e7ab2b283de8be6b1c3c
gautchap <gautchap+github@outlook.com>
2023-07-22 23:18:33 +0200

    feat : add next auth provider and api endpoint

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,44 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import SpotifyProvider from "next-auth/providers/spotify";
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        SpotifyProvider({
+            clientId: process.env.SPOTIFY_CLIENT_ID,
+            clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+            authorization: {
+                params: {
+                    scope: "user-top-read playlist-modify-public playlist-modify-private user-read-recently-played",
+                    show_dialog: true,
+                },
+            },
+        }),
+    ],
+    callbacks: {
+        async session({ session, token }) {
+            if (session) {
+                session = Object.assign({}, session, { id: token.id });
+                session = Object.assign({}, session, { access_token: token.access_token });
+            }
+            return session;
+        },
+        async jwt({ token, user, account }) {
+            if (user) {
+                token = Object.assign({}, token, { id: user.id });
+            }
+
+            if (account) {
+                token = Object.assign({}, token, { access_token: account.access_token });
+                token = Object.assign({}, token, { refresh_token: account.refresh_token });
+            }
+
+            return token;
+        },
+    },
+    pages: {
+        signIn: "/auth/signin",
+        error: "/auth/error",
+    },
+};
+
+export default NextAuth(authOptions);
