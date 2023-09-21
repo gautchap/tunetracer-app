679e27292359d443525877046291b9755861aa7b
gautchap <gautchap+github@outlook.com>
2023-09-21 11:36:52 +0200

    feat : add react query provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     return (
-        <SessionProvider session={session}>
-            <Component {...pageProps} />
-        </SessionProvider>
+        <QueryClientProvider client={queryClient}>
+            <SessionProvider session={session}>
+                <Component {...pageProps} />
+            </SessionProvider>
+            <ReactQueryDevtools />
+        </QueryClientProvider>
     );
 }
