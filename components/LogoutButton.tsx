2ce14a3ef637a91fbadde214d5b73d8bcefd45cb
gautchap <gautchap+github@outlook.com>
2023-09-30 23:27:10 +0200

    refactor : remove unused code

--- components/LogoutButton.tsx
+++
@@ -1,9 +0,0 @@
-import { signOut } from "next-auth/react";
-
-export default function LogoutButton() {
-    return (
-        <button style={{ cursor: "pointer" }} onClick={() => signOut()}>
-            Sign Out
-        </button>
-    );
-}
