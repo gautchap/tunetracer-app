dbcc659ff394272018bf6817eae12b14e1e4c430
gautchap <gautchap+github@outlook.com>
2023-09-30 22:32:24 +0200

    feat : add image type to next auth session

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -2,21 +2,24 @@ import { DefaultSession, Account as NextAuthAccount } from "next-auth";
 import { JWT as NextAuthJWT } from "next-auth/jwt";
 
 declare module "next-auth" {
-    interface Session extends DefaultSession {
-        accessToken: string;
-        error?: string;
-    }
-    interface Account extends NextAuthAccount {
-        expires_at: number;
-    }
+  interface Session extends DefaultSession {
+    accessToken: string;
+    error?: string;
+    user: {
+      image: string | undefined;
+    } & DefaultSession["user"];
+  }
+  interface Account extends NextAuthAccount {
+    expires_at: number;
+  }
 }
 
 declare module "next-auth/jwt" {
-    interface JWT extends NextAuthJWT {
-        id: string;
-        accessToken: string;
-        refreshToken: string;
-        accessTokenExpires?: number;
-        error?: "RefreshAccessTokenError";
-    }
+  interface JWT extends NextAuthJWT {
+    id: string;
+    accessToken: string;
+    refreshToken: string;
+    accessTokenExpires?: number;
+    error?: "RefreshAccessTokenError";
+  }
 }
