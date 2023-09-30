91df13e9fb4fffe43550805bab87958884bc11f9
gautchap <gautchap+github@outlook.com>
2023-09-30 22:45:40 +0200

    refactor : use ssr and accesstoken instead of session

--- components/TracksWrapper.tsx
+++ components/TracksWrapper.tsx
@@ -1,40 +1,73 @@
 import { getFavTracks } from "@/api/spotifystats";
-import { Session } from "next-auth";
 import { useQuery } from "@tanstack/react-query";
 import { Range } from "@/types/spotifyTypes";
-import TrackCard from "./TrackCard";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Link from "next/link";
 
 type TracksWrapperProps = {
-    session: Session;
-    range: Range;
+  token: string;
+  range: Range;
 };
 
-const TracksWrapper = ({ session, range }: TracksWrapperProps) => {
-    const {
-        data: tracks,
-        error,
-        isLoading,
-    } = useQuery({
-        enabled: session !== null,
-        queryKey: ["tracks", range],
-        queryFn: () => getFavTracks({ token: session.accessToken, range }),
-    });
+const TracksWrapper = ({ token, range }: TracksWrapperProps) => {
+  const {
+    data: tracks,
+    error,
+    isLoading,
+  } = useQuery({
+    enabled: token !== null,
+    queryKey: ["tracks", range],
+    queryFn: () => getFavTracks({ token, range }),
+  });
 
-    if (isLoading) return <p>loading...</p>;
-    if (error) return <p>Une erreur est survenue</p>;
+  if (isLoading) return <p>loading...</p>;
+  if (error) return <p>Une erreur est survenue</p>;
 
-    return (
-        <>
-            <section
-                style={{ width: "100%" }}
-                // style={{ display: "flex", flexWrap: "wrap", gap: "2em" }}
-            >
-                <table style={{ width: "100%" }}>
-                    <tbody>{tracks?.items.map((track) => <TrackCard key={track.id} track={track} />)}</tbody>
-                </table>
-            </section>
-        </>
-    );
+  return (
+    <>
+      <Table>
+        <TableCaption>A list of your recent listened tracks</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Ranking</TableHead>
+            <TableHead>Track&apos;s name</TableHead>
+            <TableHead>Artist(s)</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {tracks?.items.map((track, index) => (
+            <TableRow key={track.id}>
+              <TableCell className="font-medium flex items-center gap-3">
+                {index + 1}
+                <Link href={track.external_urls.spotify}>
+                  <img
+                    width={50}
+                    height={50}
+                    src={track.album.images ? track.album.images[2].url : ""}
+                    alt={track.name}
+                  />
+                </Link>
+              </TableCell>
+              <TableCell>{track.name}</TableCell>
+              <TableCell>
+                {track.artists.map((artist) => (
+                  <p key={artist.id}>{artist.name}</p>
+                ))}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
+  );
 };
 
 export default TracksWrapper;
