57378b08d20dd00201c7fc4bc11a7241824b652f
gautchap <gautchap+github@outlook.com>
2023-09-30 22:43:34 +0200

    feat : add prefetch react queries

--- api/spotifystats.ts
+++ api/spotifystats.ts
@@ -1,14 +1,48 @@
-import { FavArtists, FavItemsProps, FavTracks } from "@/types/spotifyTypes";
+import {
+  FavArtists,
+  FavItemsProps,
+  FavTracks,
+  Range,
+} from "@/types/spotifyTypes";
 import { fetcher } from "@/api/fetcher";
+import { queryClient } from "@/pages/_app";
 
-export const getFavArtists = async ({ token, range, limit = 21 }: FavItemsProps) =>
-    fetcher(`https://api.spotify.com/v1/me/top/artists?time_range=${range}&limit=${limit}`, {
-        headers: { Authorization: `Bearer ${token}` },
-        zodSchema: FavArtists,
-    });
+export const getFavArtists = async ({
+  token,
+  range,
+  limit = 21,
+}: FavItemsProps) =>
+  fetcher(
+    `https://api.spotify.com/v1/me/top/artists?time_range=${range}&limit=${limit}`,
+    {
+      headers: { Authorization: `Bearer ${token}` },
+      zodSchema: FavArtists,
+    },
+  );
 
-export const getFavTracks = async ({ token, range = "medium_term", limit = 21 }: FavItemsProps) =>
-    fetcher(`https://api.spotify.com/v1/me/top/tracks?time_range=${range}&limit=${limit}`, {
-        headers: { Authorization: `Bearer ${token}` },
-        zodSchema: FavTracks,
-    });
+export const getFavTracks = async ({
+  token,
+  range = "medium_term",
+  limit = 21,
+}: FavItemsProps) =>
+  fetcher(
+    `https://api.spotify.com/v1/me/top/tracks?time_range=${range}&limit=${limit}`,
+    {
+      headers: { Authorization: `Bearer ${token}` },
+      zodSchema: FavTracks,
+    },
+  );
+
+export const prefetchArtists = async (token: string, range: Range) => {
+  await queryClient.prefetchQuery({
+    queryKey: ["artists", range],
+    queryFn: () => getFavArtists({ token, range }),
+  });
+};
+
+export const prefetchTracks = async (token: string, range: Range) => {
+  await queryClient.prefetchQuery({
+    queryKey: ["tracks", range],
+    queryFn: () => getFavTracks({ token, range }),
+  });
+};
