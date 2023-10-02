512ecc24d5b5e422e7c66b6258e55152063e97d3
gautchap <gautchap+github@outlook.com>
2023-10-02 16:38:47 +0200

    refactor: remove unused code

--- src/components/ArtistCard.tsx
+++ src/components/ArtistCard.tsx
@@ -1,39 +1,35 @@
 import { Artist } from "@/types/spotifyTypes";
-import { useContext } from "react";
-import { MobileContext } from "@/context/MobileProvider";
 
 type ArtistCardProps = {
-  artist: Artist;
-  ranking: number;
+    artist: Artist;
+    ranking: number;
 };
 
 const ArtistCard = ({ artist, ranking }: ArtistCardProps) => {
-  const isMobile = useContext(MobileContext);
+    return (
+        <article>
+            <p className="font-bold ml-1">
+                #{ranking + 1} {artist.name}
+            </p>
 
-  return (
-    <article>
-      <p className="font-bold ml-1">
-        #{ranking + 1} {artist.name}
-      </p>
-
-      <a href={artist.external_urls.spotify}>
-        <img
-          className="rounded-2xl shadow-lg"
-          src={
-            artist.images
-              ? isMobile
-                ? artist.images[1].url
-                : artist.images[0].url
-              : ""
-          }
-          alt={artist.name}
-          width={isMobile ? 160 : 320}
-          // height={isMobile ? 160 : 320}
-          sizes="(max-width: 768px) 80vw, (max-width: 1200px) 40vw, 26vw"
-        />
-      </a>
-    </article>
-  );
+            <a href={artist.external_urls.spotify}>
+                <img
+                    className="rounded-2xl shadow-lg hidden md:block"
+                    src={artist?.images?.[0]?.url}
+                    alt={artist.name}
+                    width={320}
+                    sizes="(max-width: 768px) 80vw, (max-width: 1200px) 40vw, 26vw"
+                />
+                <img
+                    className="rounded-2xl shadow-lg block md:hidden"
+                    src={artist?.images?.[1]?.url}
+                    alt={artist.name}
+                    width={160}
+                    sizes="(max-width: 768px) 80vw, (max-width: 1200px) 40vw, 26vw"
+                />
+            </a>
+        </article>
+    );
 };
 
 export default ArtistCard;
