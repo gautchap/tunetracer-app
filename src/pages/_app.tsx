1be301e9a45634e0ad54f28cdd72e59a88056874
gautchap <gautchap+github@outlook.com>
2023-10-12 16:46:24 +0200

    feat : add toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,19 +5,29 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ThemeProvider } from "@/context/ThemeProvider";
 import { NavBar } from "@/components/NavBar";
+import { Toaster } from "@/components/ui/toaster";
 
 export const queryClient = new QueryClient();
 
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-    return (
-        <QueryClientProvider client={queryClient}>
-            <SessionProvider session={session}>
-                <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-                    <NavBar />
-                    <Component {...pageProps} />
-                </ThemeProvider>
-            </SessionProvider>
-            <ReactQueryDevtools />
-        </QueryClientProvider>
-    );
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <NavBar />
+          <Component {...pageProps} />
+          <Toaster />
+        </ThemeProvider>
+      </SessionProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
