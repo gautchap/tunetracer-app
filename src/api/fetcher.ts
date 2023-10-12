efda457bd72dbde2acc99b866b5f01473471c2fe
gautchap <gautchap+github@outlook.com>
2023-10-12 16:48:07 +0200

    refactor : remove unused code

--- src/api/fetcher.ts
+++ src/api/fetcher.ts
@@ -2,48 +2,54 @@ import { z } from "zod";
 import { signOut } from "next-auth/react";
 
 interface FetcherConfig<T> {
-    data?: unknown;
-    zodSchema?: z.ZodSchema<T>;
-    method?: "DELETE" | "GET" | "OPTIONS" | "PATCH" | "POST" | "PUT";
-    headers?: HeadersInit;
-    customConfig?: RequestInit;
+  data?: unknown;
+  zodSchema?: z.ZodSchema<T>;
+  method?: "DELETE" | "GET" | "OPTIONS" | "PATCH" | "POST" | "PUT";
+  headers?: HeadersInit;
+  customConfig?: RequestInit;
 }
 
 export async function fetcher<T>(
-    url: string,
-    { data, zodSchema, method, headers: customHeaders, customConfig }: FetcherConfig<T>
+  url: string,
+  {
+    data,
+    zodSchema,
+    method,
+    headers: customHeaders,
+    customConfig,
+  }: FetcherConfig<T>,
 ): Promise<T> {
-    const config: RequestInit = {
-        method: method ?? (data ? "POST" : "GET"),
+  const config: RequestInit = {
+    method: method ?? (data ? "POST" : "GET"),
 
-        body: data ? JSON.stringify(data) : null,
-        headers: {
-            "Content-Type": data ? "application/json" : "",
-            Accept: "application/json",
-            ...customHeaders,
-        },
-        ...customConfig,
-    };
+    body: data ? JSON.stringify(data) : null,
+    headers: {
+      "Content-Type": data ? "application/json" : "",
+      Accept: "application/json",
+      ...customHeaders,
+    },
+    ...customConfig,
+  };
 
-    // eslint-disable-next-line github/no-then
-    return window.fetch(url, config).then(async (response) => {
-        if (response.status === 401) {
-            signOut();
-            throw new Error("You're not authenticated");
-        }
+  // eslint-disable-next-line github/no-then
+  return fetch(url, config).then(async (response) => {
+    if (response.status === 401) {
+      signOut();
+      throw new Error("You're not authenticated");
+    }
 
-        let result = null;
-        try {
-            result = response.status === 204 ? null : await response.json();
-        } catch (error: unknown) {
-            // eslint-disable-next-line unicorn/no-useless-promise-resolve-reject
-            return Promise.reject(error);
-        }
+    let result = null;
+    try {
+      result = response.status === 204 ? null : await response.json();
+    } catch (error: unknown) {
+      // eslint-disable-next-line unicorn/no-useless-promise-resolve-reject
+      return Promise.reject(error);
+    }
 
-        if (response.ok) {
-            return zodSchema && result ? zodSchema.parse(result) : result;
-        } else {
-            throw result;
-        }
-    });
+    if (response.ok) {
+      return zodSchema && result ? zodSchema.parse(result) : result;
+    } else {
+      throw result;
+    }
+  });
 }
