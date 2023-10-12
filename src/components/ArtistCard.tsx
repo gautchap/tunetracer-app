fc47282a574d446970582c184b68a8c2fafafd7d
gautchap <gautchap+github@outlook.com>
2023-10-12 16:47:14 +0200

    refactor : add effect on hover

--- src/components/ArtistCard.tsx
+++ src/components/ArtistCard.tsx
@@ -1,35 +1,38 @@
 import { Artist } from "@/types/spotifyTypes";
 
 type ArtistCardProps = {
-    artist: Artist;
-    ranking: number;
+  artist: Artist;
+  ranking: number;
 };
 
 const ArtistCard = ({ artist, ranking }: ArtistCardProps) => {
-    return (
-        <article>
-            <p className="font-bold ml-1">
-                #{ranking + 1} {artist.name}
-            </p>
+  return (
+    <article>
+      <p className="font-bold ml-1">
+        #{ranking + 1} {artist.name}
+      </p>
 
-            <a href={artist.external_urls.spotify}>
-                <img
-                    className="rounded-2xl shadow-lg hidden md:block"
-                    src={artist?.images?.[0]?.url}
-                    alt={artist.name}
-                    width={320}
-                    sizes="(max-width: 768px) 80vw, (max-width: 1200px) 40vw, 26vw"
-                />
-                <img
-                    className="rounded-2xl shadow-lg block md:hidden"
-                    src={artist?.images?.[1]?.url}
-                    alt={artist.name}
-                    width={160}
-                    sizes="(max-width: 768px) 80vw, (max-width: 1200px) 40vw, 26vw"
-                />
-            </a>
-        </article>
-    );
+      <a
+        href={artist.external_urls.spotify}
+        className="inline-block overflow-hidden rounded-2xl"
+      >
+        <img
+          className="shadow-lg hidden md:block hover:scale-110 transition"
+          src={artist?.images?.[0]?.url}
+          alt={artist.name}
+          width={320}
+          sizes="(max-width: 768px) 80vw, (max-width: 1200px) 40vw, 26vw"
+        />
+        <img
+          className="shadow-lg block md:hidden hover:scale-125 transition"
+          src={artist?.images?.[1]?.url}
+          alt={artist.name}
+          width={160}
+          sizes="(max-width: 768px) 80vw, (max-width: 1200px) 40vw, 26vw"
+        />
+      </a>
+    </article>
+  );
 };
 
 export default ArtistCard;
