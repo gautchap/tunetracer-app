34dae244369a6aa51d96e96f4cd6192b1e0632e0
gautchap <gautchap+github@outlook.com>
2024-01-11 14:52:08 +0100

    refactor : add player bar but not for playing page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,32 +7,32 @@ import { ThemeProvider } from "@/context/ThemeProvider";
 import { NavBar } from "@/components/NavBar";
 import { Toaster } from "@/components/ui/toaster";
 import { Inter } from "next/font/google";
+import PlayerProvider from "@/context/PlayerProvider";
+import { useRouter } from "next/router";
+import SidePlayer from "@/components/SidePlayer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const queryClient = new QueryClient();
 
-export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
-}: AppProps) {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <SessionProvider session={session}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <div className={inter.className}>
-            <NavBar />
-            <Component {...pageProps} />
-            <Toaster />
-          </div>
-        </ThemeProvider>
-      </SessionProvider>
-      <ReactQueryDevtools />
-    </QueryClientProvider>
-  );
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+    const router = useRouter();
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <SessionProvider session={session}>
+                <PlayerProvider>
+                    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+                        <div className={inter.className}>
+                            <NavBar />
+                            <Component {...pageProps} />
+                            <Toaster />
+                            {router.pathname !== "/playing" && <SidePlayer />}
+                        </div>
+                    </ThemeProvider>
+                </PlayerProvider>
+            </SessionProvider>
+            <ReactQueryDevtools />
+        </QueryClientProvider>
+    );
 }
