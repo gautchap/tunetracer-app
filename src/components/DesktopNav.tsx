6940d2d870a8c953a5a9fa2590abaf9e5904487b
gautchap <gautchap+github@outlook.com>
2024-01-11 14:53:42 +0100

    feat : add playing page to navbar

--- src/components/DesktopNav.tsx
+++ src/components/DesktopNav.tsx
@@ -1,40 +1,66 @@
 import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  navigationMenuTriggerStyle,
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
+import { signIn, useSession } from "next-auth/react";
 import Link from "next/link";
+import { useToast } from "@/components/ui/use-toast";
+import { ToastAction } from "@/components/ui/toast";
 
 export function DesktopNav() {
-  return (
-    <div className="hidden md:flex">
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <Link href="/" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Home
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="/tracks" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Tracks
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="/artists" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Artists
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-    </div>
-  );
+    const { data: session } = useSession();
+    const { toast } = useToast();
+
+    const handleClick = () => {
+        if (!session) {
+            toast({
+                variant: "destructive",
+                title: "Uh oh! Something went wrong.",
+                description: "You need to log in before access this page.",
+                action: (
+                    <ToastAction onClick={() => signIn("spotify")} altText="Try again">
+                        Log In
+                    </ToastAction>
+                ),
+            });
+        }
+    };
+
+    return (
+        <div className="hidden md:flex">
+            <NavigationMenu>
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <Link href="/" legacyBehavior passHref>
+                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>Home</NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Link href="/tracks" legacyBehavior passHref>
+                            <NavigationMenuLink onClick={handleClick} className={navigationMenuTriggerStyle()}>
+                                Tracks
+                            </NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Link href="/artists" legacyBehavior passHref>
+                            <NavigationMenuLink onClick={handleClick} className={navigationMenuTriggerStyle()}>
+                                Artists
+                            </NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Link href="/playing" legacyBehavior passHref>
+                            <NavigationMenuLink onClick={handleClick} className={navigationMenuTriggerStyle()}>
+                                Playing
+                            </NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+        </div>
+    );
 }
--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,48 +1,70 @@
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { signIn, useSession } from "next-auth/react";
 import Link from "next/link";
+import { useToast } from "@/components/ui/use-toast";
+import { ToastAction } from "@/components/ui/toast";
 
 export function MobileNav() {
-  return (
-    <div className="flex items-center md:hidden">
-      <Sheet>
-        <SheetTrigger asChild>
-          <button className="h-5 w-5">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="1em"
-              viewBox="0 0 448 512"
-              className="fill-current bg-background text-foreground h-full w-full"
-            >
-              <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
-            </svg>
-          </button>
-        </SheetTrigger>
-        <SheetContent side="left" className="p-10">
-          <SheetHeader className="text-left">
-            <SheetTitle>
-              <SheetClose asChild>
-                <Link href="/">Home</Link>
-              </SheetClose>
-            </SheetTitle>
-            <hr />
-          </SheetHeader>
-          <div className="flex flex-col gap-2 pt-2">
-            <SheetClose asChild>
-              <Link href="/tracks">Tracks</Link>
-            </SheetClose>
-            <SheetClose asChild>
-              <Link href="/artists">Artists</Link>
-            </SheetClose>
-          </div>
-        </SheetContent>
-      </Sheet>
-    </div>
-  );
+    const { data: session } = useSession();
+    const { toast } = useToast();
+
+    const handleClick = () => {
+        if (!session) {
+            toast({
+                variant: "destructive",
+                title: "Uh oh! Something went wrong.",
+                description: "You need to log in before access this page.",
+                action: (
+                    <ToastAction onClick={() => signIn("spotify")} altText="Try again">
+                        Log In
+                    </ToastAction>
+                ),
+            });
+        }
+    };
+    return (
+        <div className="flex items-center md:hidden">
+            <Sheet>
+                <SheetTrigger asChild>
+                    <button className="h-5 w-5">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="1em"
+                            viewBox="0 0 448 512"
+                            className="fill-current bg-background text-foreground h-full w-full"
+                        >
+                            <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+                        </svg>
+                    </button>
+                </SheetTrigger>
+                <SheetContent side="left" className="p-10">
+                    <SheetHeader className="text-left">
+                        <SheetTitle>
+                            <SheetClose asChild>
+                                <Link href="/">Home</Link>
+                            </SheetClose>
+                        </SheetTitle>
+                        <hr />
+                    </SheetHeader>
+                    <div className="flex flex-col gap-2 pt-2">
+                        <SheetClose asChild>
+                            <Link onClick={handleClick} href="/tracks">
+                                Tracks
+                            </Link>
+                        </SheetClose>
+                        <SheetClose asChild>
+                            <Link onClick={handleClick} href="/artists">
+                                Artists
+                            </Link>
+                        </SheetClose>
+                        <SheetClose asChild>
+                            <Link onClick={handleClick} href="/playing">
+                                Playing
+                            </Link>
+                        </SheetClose>
+                    </div>
+                </SheetContent>
+            </Sheet>
+        </div>
+    );
 }
